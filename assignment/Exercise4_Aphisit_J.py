241a77926f766c033dd0eec14894876a10d4acbb
SIX0MA <omsinloveha@gmail.com>
2023-05-28 19:18:51 +0700

    Exercise4_Aphisit_J.py

---
+++ assignment/Exercise4_Aphisit_J.py
@@ -0,0 +1,10 @@
+FOUN_ENG = 85.2
+GEN_BUS = 90.5
+INTRO_COM_SYS = 85
+COM_PRO = 90
+
+print("---Your Score---")
+print("Foundation English : ",FOUN_ENG)
+print("General Business : ",GEN_BUS)
+print("Introduction to Computer Systems : ",INTRO_COM_SYS)
+print("Computer Programing",COM_PRO)
